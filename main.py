adf3c5e9d689f2091569e8a1d99001229b012f8d
anvarbekxabibullayev764-cmd <anvarbekxabibullayev764@gmail.com>
2026-04-03 14:51:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ os.makedirs(TEMPLATE_DIR, exist_ok=True)
 os.makedirs(CERT_DIR, exist_ok=True)
 
 TEMPLATES = {
-    "template1": {"file": f"{TEMPLATE_DIR}/template1.png", "x": 350, "y": 530, "size": 110, "color": (0, 0, 0)},
-    "template2": {"file": f"{TEMPLATE_DIR}/template2.png", "x": 400, "y": 315, "size": 100, "color": (0, 51, 102)},
-    "template3": {"file": f"{TEMPLATE_DIR}/template3.png", "x": 310, "y": 468, "size": 120, "color": (0, 80, 0)},
-    "template4": {"file": f"{TEMPLATE_DIR}/template4.png", "x": 420, "y": 343, "size": 110, "color": (0, 100, 0)},
-    "template5": {"file": f"{TEMPLATE_DIR}/template5.png", "x": 455, "y": 420, "size": 110, "color": (0, 70, 0)},
-    "template6": {"file": f"{TEMPLATE_DIR}/template6.png", "x": 400, "y": 340, "size": 110, "color": (0, 0, 0)},
+    "template1": {"file": f"{TEMPLATE_DIR}/template1.png", "x": 350, "y": 530, "size": 160, "color": (0, 0, 0)},
+    "template2": {"file": f"{TEMPLATE_DIR}/template2.png", "x": 400, "y": 315, "size": 150, "color": (0, 51, 102)},
+    "template3": {"file": f"{TEMPLATE_DIR}/template3.png", "x": 310, "y": 468, "size": 170, "color": (0, 80, 0)},
+    "template4": {"file": f"{TEMPLATE_DIR}/template4.png", "x": 420, "y": 343, "size": 160, "color": (0, 100, 0)},
+    "template5": {"file": f"{TEMPLATE_DIR}/template5.png", "x": 455, "y": 420, "size": 160, "color": (0, 70, 0)},
+    "template6": {"file": f"{TEMPLATE_DIR}/template6.png", "x": 400, "y": 340, "size": 160, "color": (0, 0, 0)},
 }
 
 class Form(StatesGroup):
